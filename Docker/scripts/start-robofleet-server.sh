a6ea4563d1c1ed27e80e991a63867f42fd7c50a0
frank-Regal <fregal@utexas.edu>
2024-10-14 13:23:47 -0500

    updated robotfleet server

--- Docker/scripts/start-robofleet-server.sh
+++ Docker/scripts/start-robofleet-server.sh
@@ -23,14 +23,32 @@ get_local_ip() {
     echo $local_ip
 }
 
-
-# Create or attach to "robofleet" tmux session and run yarn start
-create_or_attach_tmux robofleet-server "cd /deps/robofleet/robofleet_server && sudo yarn start"
+# Function to update config.ts with the IP range
+update_config() {
+    local ip=$1
+    local config_file="/deps/robofleet/robofleet_server/src/config.ts"
+    
+    # Extract the subnet
+    local subnet=$(echo $ip | cut -d. -f1-3)
+    
+    # Create the new line to add
+    local new_line="{ipRange: [\"$subnet.1\", \"$subnet.255\"], allow: [\"send\", \"receive\"]},"
+    
+    # Use sed to add the new line after the permissions array opening
+    sudo sed -i "/permissions: \[/a \ \ \ \ \ \ \ \ $new_line" $config_file
+    
+    echo "Updated config.ts with IP range: $subnet.1 to $subnet.255"
+}
 
 # Get the local IP address
 ip_address=$(get_local_ip)
 
+# Update the config.ts file with the IP range
+update_config $ip_address
+
+# Create or attach to "robofleet" tmux session and run yarn start
+create_or_attach_tmux robofleet-server "cd /deps/robofleet/robofleet_server && sudo yarn start"
+
 # Print the IP address and start message
 echo "Robofleet server started on IP: $ip_address"
 echo "To view the status of the server, run: 'tmux attach-session -t robofleet-server'"
-
